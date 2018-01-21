3a573ea22a5197310d42a5b06d0264e1da4f1c40
Anton Krug <antonkrug@gmail.com>
2018-01-21 14:45:12 +0000

    Checkboard pattern with DoF

--- src/scenes/lights.cpp
+++ src/scenes/lights.cpp
@@ -10,24 +10,24 @@
 Lights::Lights(): Scene(2, 2) {
 
   camera = {
-    .possition    = Vector3(0.0f, 0.0f,  0.0f),
-    .lookAt       = Vector3(0.0f, 10.0f, 60.0f),
-    .apeture      = 0.0f,
+    .possition    = Vector3(0.0f, 0.0f,  -20.0f),
+    .lookAt       = Vector3(0.0f, 10.0f, 30.0f),
+    .apeture      = 4.0f,
     .shutterSpeed = 0.5f
   };
 
 
   lights[0] = LightOmniGen([](float frame) {
     const float lightRotate = (M_PI * frame) / 31;
-    Vector3 center(320 * cosf(lightRotate), 10 * (sinf(lightRotate) - 20 ), 20);
-    Color color(0.1f, 0.0f, 0.7f);
+    Vector3 center(320 * cosf(lightRotate), 10 * (sinf(lightRotate) - 20 ), -20);
+    Color color(0.1f, 0.0f, 0.4f);
 
     return LightOmni(center, color);
   });
 
   lights[1] = LightOmniGen([](float frame) {
-    Vector3 center(300, -50, 200);
-    Color color(0.0f, 0.8f, 0.0f);
+    Vector3 center(80, -50, 100);
+    Color color(0.2f, 0.9f, 0.0f);
 
     return LightOmni(center, color);
   });
@@ -50,9 +50,9 @@ Lights::Lights(): Scene(2, 2) {
 
 
   objects[1] = SphereGen([](float frame) {
-    return Sphere(Vector3(sinf(frame / 8) * 15, sin(frame / 4) * 2 + 9, sin(frame / 20) * 15 + 25), 10,
+    return Sphere(Vector3(sinf(frame / 8) * 15, sin(frame / 4) * 2 +3, sin(frame / 20) * 15 + 25), 15,
                   [](Vector3 point, float frame) {
-                    return Materials::red;
+                    return Materials::brass;
                   });
   });
 
