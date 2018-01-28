f8ebe97ec2e64020106cac28ff247b12aa0aebc3
Anton Krug <antonkrug@gmail.com>
2018-01-28 17:01:49 +0000

    removed old naming

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -170,17 +170,17 @@ void Render::renderPartialWindow(float frame, windowType window) {
       while (sampler.isNext()) {
 
         sampler.getNextSample(&sample);
-        float recenterX = ( x + sample.spaceX - width/2.0f)  / ( 2.0 * width);
-        float recenterY = ( y + sample.spaceY - height/2.0f) / ( 2.0 * height);
+        const float recenterX = ( x + sample.spaceX - width  / 2.0f ) / ( 2.0 * width  );
+        const float recenterY = ( y + sample.spaceY - height / 2.0f ) / ( 2.0 * height );
 
         Vector3 down(0.0f, -1.0f, 0.0f);
         Vector3 start  = scene->camera.possition + Vector3(sample.lensX, sample.lensY, 0);
         Vector3 lookAt = scene->camera.lookAt;
-        Vector3 right  = (~Vector3(lookAt & down))*320;
-        Vector3 up2    = (~Vector3(lookAt & right))*200;
-        Vector3 dest3  = ~Vector3((right * recenterX) + (up2 * recenterY) + lookAt - start);
+        Vector3 right  = (~Vector3(lookAt & down))  * 320;
+        Vector3 up     = (~Vector3(lookAt & right)) * 200;
+        Vector3 dest   = ~Vector3((right * recenterX) + (up * recenterY) + lookAt - start);
 
-        Ray rayForThisPixel(start,dest3);
+        Ray rayForThisPixel(start, dest);
         light = scene->lights[sample.light].evaluateFn(frame);
         lastColor = rayStart(rayForThisPixel, objects, &light, frame + sample.time);
 
