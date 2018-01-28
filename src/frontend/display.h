e77050d5324a7dbec286e214ee13847d9ac8800e
Anton Krug <antonkrug@gmail.com>
2018-01-28 15:18:33 +0000

    easier to switch complexities

--- src/frontend/display.h
+++ src/frontend/display.h
@@ -6,21 +6,34 @@
 #ifndef RAYTRACER_DISPLAY_H
 #define RAYTRACER_DISPLAY_H
 
-//#define WIDTH  160
-//#define HEIGHT 100
-//#define SCALE 8.0  // display the window bigger than it really is (render low-res while having large window)
+#define RENDER_COMPLEXITY 2
 
-#define WIDTH  320
-#define HEIGHT 200
-#define SCALE 4.0  // display the window bigger than it really is (render low-res while having large window)
+#if RENDER_COMPLEXITY == 1
+  #define WIDTH  160
+  #define HEIGHT 100
+  #define SCALE 8.0  // display the window bigger than it really is (render low-res while having large window)
 
-//#define WIDTH  640
-//#define HEIGHT 400
-//#define SCALE 2.0  // display the window bigger than it really is (render low-res while having large window)
+#elif RENDER_COMPLEXITY == 2
+  #define WIDTH  320
+  #define HEIGHT 200
+  #define SCALE 4.0  // display the window bigger than it really is (render low-res while having large window)
 
-//#define WIDTH  960
-//#define HEIGHT 600
-//#define SCALE 1.0
+#elif RENDER_COMPLEXITY == 3
+  #define WIDTH  640
+  #define HEIGHT 400
+  #define SCALE 2.0  // display the window bigger than it really is (render low-res while having large window)
+
+#elif RENDER_COMPLEXITY == 4
+  #define WIDTH  960
+  #define HEIGHT 600
+  #define SCALE 1.0
+
+#elif RENDER_COMPLEXITY == 5
+  #define WIDTH  1280
+  #define HEIGHT 800
+  #define SCALE 1.0
+
+#endif
 
 #include <SFML/Graphics.hpp>
 #include "../render/render.h"
