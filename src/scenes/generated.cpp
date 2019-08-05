16d211892ee737e66560fa97a21316fb426599e2
Anton Krug <antonkrug@gmail.com>
2019-08-05 15:49:38 +0100

    More diverse scene

--- src/scenes/generated.cpp
+++ src/scenes/generated.cpp
@@ -10,22 +10,45 @@
 
 Generated::Generated(): SceneGenerator() {
   camera = {
-    .position    = Vector3(0.2f, 1.0f, 0.3f),
-    .lookAt      = Vector3(0.0f, 0.0f, 60.0f),
-    .aperture    = 8.0f,
+    .position    = Vector3(0.1f, 0.2f, 0.3f),
+    .lookAt      = Vector3(0.0f, 10.0f, 60.0f),
+    .aperture    = 0.0f,
     .shutterBlur = 0.3f
   };
 
   lightEnd = 2.0f;
+  lastFrame = 130.0f;
 
-  //  auto onlyLight = new LightOmniGenerator(Vector3(32, 0.6, 20), Color(0.9f, 0.9f, 1.0f));
-  lightGenerators->push_back(new LightOmniGenerator(Vector3(32, 0.6, 20), Color(0.9f, 0.9f, 1.0f)));
+//  auto animatedLight = new LightOmniGenerator(Vector3(32, 0.6, 20), Color(0.9f, 0.9f, 1.0f));
+//  auto animatedLight = new LightOmniGenerator([](float frame) {
+//    const float lightRotate = (M_PI * frame) / 31;
+//    Vector3 center(320 * cosf(lightRotate), 0.6 * 200 * (sinf(lightRotate) - 0.5), 20);
+//    Color color(0.5f, 0.5f, 0.9f);
+//
+//    return new LightOmni(center, color);
+//  });
+//  lightGenerators->push_back(animatedLight);
 
-  auto sphere = new SphereGenerator(Vector3(), 10.0f);
-  objectGenerators->push_back(sphere);
-  std::cout << "Made\r\n";
+  lightGenerators->push_back(new LightOmniGenerator(Vector3(600, -50, 200),Color(0.0f, 0.8f, 0.0f) ));
+
+
+  objectGenerators->push_back(new SphereGenerator([](float frame) {
+    return new Sphere(Vector3(0.0f, 0.0f, 100.0f), 25.0f, Materials::green);
+  }));
+
+  objectGenerators->push_back(new SphereGenerator([](float frame) {
+    return new Sphere(Vector3(sinf(frame / 8) * 15,
+                              sinf(frame / 4) * 2 + 9,
+                              sinf(frame / 20) * 45 + 65),
+                     10,
+                     [](Vector3 point, float frame) {
+                        return Materials::red;
+                     });
+  }));
+
+  std::cout << "Made sceneGenerator 'generated'\r\n";
 }
 
 Generated::~Generated() {
-  std::cout << "Destroying\r\n";
+  std::cout << "Destroying sceneGenerator 'generated'\r\n";
 }
